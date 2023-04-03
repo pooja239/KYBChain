905ab2a2fef5fe4b88ec71e6c47b330c4855fb98
Pooja Gupta <58245210+pooja239@users.noreply.github.com>
2023-04-03 12:53:35 +1000

    Add files via upload

---
+++ caliper-benchmarks/scenario/registerSc_write/utils/simple-state.js
@@ -0,0 +1,115 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+'use strict';
+
+const Dictionary = 'abcdefghijklmnopqrstuvwxyz';
+const Web3 = require("web3");
+
+/**
+ * Class for managing simple account states.
+ */
+class SimpleState {
+
+    /**
+     * Initializes the instance.
+     */
+    constructor(workerIndex, privacyRatingSc, _index = 0, subscriptions=0) {
+        this.privacyRatingSc = privacyRatingSc;
+        this.subscriptionsGenerated = subscriptions;
+        this.indexGenerated = _index;
+    }
+
+    getinitaddress() {
+        return {
+            paymentRatingScAddress: this.privacyRatingSc,
+        };
+    }
+
+    getregisterBuyer() {
+        this.indexGenerated++;
+        let user = "Buyer" + this.indexGenerated;
+        return {
+            username: user,
+            role: 1,
+            balance: 1000000000000
+        };
+    }
+
+    getregisterSeller() {
+        this.indexGenerated++;
+        let user = "Seller" + this.indexGenerated;
+        //let user = "Seller"
+        return {
+            username: user,
+            _role: 0,
+            _balance: 100000000000
+        };
+    }
+
+    getregisterAgreement() {
+        this.indexGenerated++;
+        let seller = "Seller" + this.indexGenerated;
+        let buyer = "Buyer" + this.indexGenerated;
+        const web3 = new Web3;
+        let AID = web3.utils.soliditySha3(web3.eth.abi.encodeParameters(['string','string'],[buyer, seller]));
+
+        return {
+            _seller: seller,
+            _buyer: buyer
+        };
+    }
+
+    getaddSubscription(index) {
+        let seller = "Seller" + index;
+        let buyer = "Buyer" + index;
+        this.subscriptionsGenerated++;
+        const web3 = new Web3;
+        let AID = web3.utils.soliditySha3(web3.eth.abi.encodeParameters(['string','string'],[buyer, seller]));
+        //console.log(">>>>>>>>>>>>>>>>" + buyer + "   " + seller)
+        //console.log(">>>>>>>>>>>>>>>>" + this.subscriptionsGenerated)
+        return {
+            _AID: AID,
+            time: 1,
+            dataType: 1,
+            interval: 1,
+            period: 1,
+            age: 1,
+            samples: 1,
+            dim: 1,
+            price: 1,
+            resell: true
+        };
+    }
+
+
+    getconfirmDelivery(index) {
+        let seller = "Seller" + index;
+        let buyer = "Buyer" + index;
+        const web3 = new Web3;
+        let SID = web3.utils.soliditySha3(this.subscriptionsGenerated);
+        let AID = web3.utils.soliditySha3(web3.eth.abi.encodeParameters(['string','string'],[buyer, seller]));
+        //console.log(">>>>>>>>>>>>>>>>" + buyer + "   " + seller)
+        //console.log(">>>>>>>>>>>>>>>>" + this.subscriptionsGenerated)
+        this.subscriptionsGenerated++;
+        return {
+            _buyer: buyer,
+            _AID: AID,
+            _SID: SID
+        };
+    }
+    
+}
+
+module.exports = SimpleState;
